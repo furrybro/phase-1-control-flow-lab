4f7b7854c4078bd0e178d6bfdf948724c5bbace2
Jeena Park <jeenapark15@gmail.com>
2022-04-25 22:11:40 -0400

    commit tree 04f05bddb5e5bccb464d8cced8464d9c6c8aec16

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number <= 400) {
+    return 'This one is on me!';
+  } else if (number > 400 && number < 2000) {
+    return 'That will be twenty bucks.';
+  } else if (number > 2000 && number < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (number > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let tipMessage;
+
+  switch (tip) {
+    case 'generous':
+      tipMessage = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipMessage = 'Thank you.';
+      break;
+    default:
+      tipMessage = 'Bye.';
+  }
+  return tipMessage;
 }
